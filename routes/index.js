b33b924f0c8e5d40887c085c03b51dda3c3e3f88
Katarzyna Kaswen-Wilk <kml.wilk@gmail.com>
2020-05-06 18:42:25 +0200

    add new message page and posting

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,13 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Mini Messageboard', messages: messages });
 });
 
+router.get('/new', function (req, res, next) {
+  res.render('form', { title: 'New Message' });
+});
+
+router.post('/new', function (req, res, next) {
+  messages.push({ text: req.body.message, user: req.body.name, added: new Date() });
+  res.redirect('/');
+});
+
 module.exports = router;
